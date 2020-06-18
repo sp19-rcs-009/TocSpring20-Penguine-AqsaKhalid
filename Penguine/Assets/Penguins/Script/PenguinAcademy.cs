3e79d8afe0d26b70d3fbee630add880e32eb1988
sp19-rcs-009 <65023965+sp19-rcs-009@users.noreply.github.com>
2020-06-18 14:24:33 +0500

    updated
    updated

---
+++ Penguine/Assets/Penguins/Script/PenguinAcademy.cs
@@ -0,0 +1,35 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinAcademy : Academy
+{
+    /// <summary>
+    /// Gets/sets the current fish speed
+    /// </summary>
+    public float FishSpeed { get; private set; }
+
+    /// <summary>
+    /// Gets/sets the current acceptable feed radius
+    /// </summary>
+    public float FeedRadius { get; private set; }
+
+    public override void InitializeAcademy()
+    {
+        FishSpeed = 0f;
+        FeedRadius = 0f;
+
+        // Set up code to be called every time the fish_speed parameter changes 
+        // during curriculum learning
+        FloatProperties.RegisterCallback("fish_speed", f =>
+        {
+            FishSpeed = f;
+        });
+        FloatProperties.RegisterCallback("feed_radius", f =>
+        {
+            FeedRadius = f;
+        });
+
+    }
+}
